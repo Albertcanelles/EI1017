5f63962de8abcafd3212f61931fa03a1b60ba747
Extrauren <36074359+Extrauren@users.noreply.github.com>
2018-02-27 10:57:55 +0100

    Constructores cliente empresa y particular

--- src/main/java/uji/es/EI1017/clases/Cliente.java
+++ src/main/java/uji/es/EI1017/clases/Cliente.java
@@ -1,4 +1,20 @@
 package uji.es.EI1017.clases;
 
+import java.util.Date;
+
 public class Cliente {
+
+    private String nombre;
+    private String nif;
+    private String email;
+    private Date fechaAlta;
+    private double tarifa;
+
+    public Cliente(String nombre, String nif, String email, Date fechaAlta, double tarifa) {
+        this.nombre = nombre;
+        this.nif = nif;
+        this.email = email;
+        this.fechaAlta = fechaAlta;
+        this.tarifa = tarifa;
+    }
 }
--- src/main/java/uji/es/EI1017/herencias/Empresa.java
+++ src/main/java/uji/es/EI1017/herencias/Empresa.java
@@ -1,4 +1,12 @@
 package uji.es.EI1017.herencias;
 
-public class Empresa {
+import uji.es.EI1017.clases.Cliente;
+
+import java.util.Date;
+
+public class Empresa extends Cliente{
+
+    public Empresa(String nombre, String nif, String email, Date fechaAlta, double tarifa) {
+        super(nombre, nif, email, fechaAlta, tarifa);
+    }
 }
