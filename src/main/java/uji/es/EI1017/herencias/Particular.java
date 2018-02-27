e8a108d0f4e7d4e498a2ade31ae984a033635a67
Extrauren <36074359+Extrauren@users.noreply.github.com>
2018-02-27 10:55:37 +0100

    Constructores cliente empresa y particular

--- src/main/java/uji/es/EI1017/herencias/Particular.java
+++ src/main/java/uji/es/EI1017/herencias/Particular.java
@@ -1,4 +1,14 @@
 package uji.es.EI1017.herencias;
 
-public class Particular {
+import uji.es.EI1017.clases.Cliente;
+
+import java.util.Date;
+
+public class Particular extends Cliente {
+
+    private String apellidos;
+    public Particular(String nombre, String nif, String email, Date fechaAlta, double tarifa, String apellidos) {
+        super(nombre, nif, email, fechaAlta, tarifa);
+        this.apellidos= apellidos;
+    }
 }
