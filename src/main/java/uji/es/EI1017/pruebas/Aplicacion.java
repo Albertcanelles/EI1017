a54df5caaa940fd5cce7bc525ff82ce431fa6b76
Albert <albertcanelles@iesebre.com>
2018-05-20 13:39:32 +0200

    ENHORABUENA YA GUARDAMOS VERSION FINAL

--- src/main/java/uji/es/EI1017/pruebas/Aplicacion.java
+++ src/main/java/uji/es/EI1017/pruebas/Aplicacion.java
@@ -10,12 +10,5 @@ public class Aplicacion {
         GuardadoDatos data = new GuardadoDatos();
         Ventana ventana = data.lectura();
         ventana.ventanaPrincipal();
-        System.out.println("vamos a guardar");
-        try {
-
-            data.escritura(ventana);
-        }catch (IOException e){
-            System.out.println("error al guardar los datos");
-        }
     }
 }
